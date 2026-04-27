9b7fdaa5deb22eb9bf7383d669dc2404b83e97aa
gopikamanojk3 <gopika.manojk3@gmail.com>
2026-04-27 09:14:22 +0530

    UC1

---
+++ TrainConsistManagementApp/App/src/TrainConsistManagementApp.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" UC2 - Add Passenger Bogies to Train ");
+        System.out.println("====================================\n");
+
+        // Create ArrayList for passenger bogies
+        ArrayList<String> passengerBogies = new ArrayList<>();
+
+        // Add bogies
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        // Display after adding
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        // Remove a bogie (AC Chair)
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nAfter Removing 'AC Chair':");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        // Check if 'Sleeper' exists
+        System.out.println("\nChecking if 'Sleeper' exists:");
+        boolean exists = passengerBogies.contains("Sleeper");
+        System.out.println("Contains Sleeper? : " + exists);
+
+        // Final consist
+        System.out.println("\nFinal Train Passenger Consist:");
+        System.out.println(passengerBogies);
+
+        System.out.println("\nUC2 operations completed successfully...");
+    }
+}
